9e5360d9c785062ee4fd55cfa03c9e08bd38b6dc
Philip Hourigan <Philip Hourigan>
2016-11-01 11:27:29 +0000

    Some changes to CD.java

--- CD.java
+++ CD.java
@@ -7,8 +7,25 @@ class CD{
 		public CD(String art){
 			artist = art;
 		}
+		
+		public static void setSong(CD obj){
+			int quitVer = 0;	
+			String theString;
+			
+			while(quitVer != 1){
+			Scanner input = new Scanner(System.in);
+			System.out.printf("Please enter the name of the song ?");
+			theString = input.nextLine();
+			obj.song.add(theString);
+			System.out.println("Enter 0 to add another song or press 1 to quit! ");
+			theString = input.nextLine();
+			quitVer = Integer.parseInt(theString);
+			}// end while	
+		}// end setSong
 	
 	public static void main(String[] args){
+		
+		
 		CD now = new CD("Mix");
 		now.song.add("War Pigs");
 		now.song.add("Homer");
@@ -16,7 +33,22 @@ class CD{
 		System.out.println(now.song.get(0));
 				for (String temp : now.song) {
 			System.out.println(temp);
-		}
+		}// end for 
+		
+		setSong(now);
+		/*
+		while(quitVer != 1){
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Please enter the name of the song ?");
+		theString = input.nextLine();
+		setSong(theString);
+		System.out.println("Enter 0 to add another song or press 1 to quit! ");
+		theString = input.nextLine();
+		quitVer = Integer.parseInt(theString);
+		
+		}*/
+		System.out.println(now.song);
+		
 		
 	}// end main
-}
+}// end CD
